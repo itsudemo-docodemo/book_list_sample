e2036ad9ab5b3c10f4ab51d8301e7e55b424bbe9
itsudemo-docodemo <itsudemo.docodemo@gmail.com>
2021-12-16 14:40:37 +0900

    1回だけ取得に変更（Stream→Get）

--- lib/book_list/book_list_model.dart
+++ lib/book_list/book_list_model.dart
@@ -3,30 +3,26 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class BookListModel extends ChangeNotifier {
-  //collectionが変化したら発火するStreamを定義
-  final Stream<QuerySnapshot> _usersStream =
-      FirebaseFirestore.instance.collection('books').snapshots();
-
   List<Book>? books;
 
   //変化をlistenする
-  void fetchBookList() {
-    //listenして変化があったらsnapshotに値が入る
-    _usersStream.listen((QuerySnapshot snapshot) {
-      //snapshotのdocumentをmap(変換)する（DocumentSnapshot型⇨Book型）
-      final List<Book> books = snapshot.docs.map((DocumentSnapshot document) {
-        //まずはString,dynamicのdataに変換する
-        Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
-        //dataからtitleとauthorを取り出す
-        final String title = data['title'];
-        final String author = data['author'];
-        //Bookにしてmapの中でリターンする
-        return Book(title, author);
-      }).toList();
-      //this.booksに格納して、完了を通知(notifyListeners)。
-      //すると、book_list_pageのConsumerが発火する。
-      this.books = books;
-      notifyListeners();
-    });
+  void fetchBookList() async {
+    //getしてsnapshotoに値が入る。
+    final QuerySnapshot snapshot =
+        await FirebaseFirestore.instance.collection('books').get();
+    //snapshotのdocumentをmap(変換)する（DocumentSnapshot型⇨Book型）
+    final List<Book> books = snapshot.docs.map((DocumentSnapshot document) {
+      //まずはString,dynamicのdataに変換する
+      Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+      //dataからtitleとauthorを取り出す
+      final String title = data['title'];
+      final String author = data['author'];
+      //Bookをinitializeしてリターンする
+      return Book(title, author);
+    }).toList();
+    //this.booksに格納して、完了を通知(notifyListeners)。
+    //すると、book_list_pageのConsumerが発火する。
+    this.books = books;
+    notifyListeners();
   }
 }
