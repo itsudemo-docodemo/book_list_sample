f680b95f65f0c37ec58b586fab5cb483720c6f14
itsudemo-docodemo <itsudemo.docodemo@gmail.com>
2021-12-16 16:12:35 +0900

    firestoreに値を追加する処理を追加

---
+++ lib/add_book/add_book_page.dart
@@ -0,0 +1,68 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'add_book_model.dart';
+
+class AddBookPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<AddBookModel>(
+      create: (_) => AddBookModel(),
+      child: Scaffold(
+        appBar: AppBar(title: Text('本を追加')),
+        body: Center(
+          //fetchBookList完了によって発火する。
+          child: Consumer<AddBookModel>(builder: (context, model, child) {
+            return Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: '本のタイトル',
+                    ),
+                    onChanged: (text) {
+                      model.title = text;
+                    },
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: '本の著者',
+                    ),
+                    onChanged: (text) {
+                      model.author = text;
+                    },
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      //追加の処理
+                      try {
+                        await model.addBook();
+                        //画面を閉じる
+                        Navigator.of(context).pop(true);
+                      } catch (e) {
+                        //snackbarを表示
+                        final snackBar = SnackBar(
+                          backgroundColor: Colors.red,
+                          content: Text(e.toString()),
+                        );
+                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                      }
+                    },
+                    child: Text('追加する'),
+                  ),
+                ],
+              ),
+            );
+          }),
+        ),
+      ),
+    );
+  }
+}
--- lib/book_list/book_list_page.dart
+++ lib/book_list/book_list_page.dart
@@ -1,3 +1,4 @@
+import 'package:book_list_sample/add_book/add_book_page.dart';
 import 'package:book_list_sample/book_list/book_list_model.dart';
 import 'package:book_list_sample/domain/book.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -34,11 +35,33 @@ class BookListPage extends StatelessWidget {
             );
           }),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: null,
-          tooltip: 'Increment',
-          child: const Icon(Icons.add),
-        ), // This trailing comma makes auto-formatting nicer for build methods.
+        floatingActionButton:
+            Consumer<BookListModel>(builder: (context, model, child) {
+          return FloatingActionButton(
+            onPressed: () async {
+              //画面遷移
+              final bool? added = await Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => AddBookPage(),
+                  fullscreenDialog: true, //pageが下から出現
+                ),
+              );
+              //追加成功したらsnackbarを表示
+              if (added != null && added == true) {
+                final snackBar = SnackBar(
+                  backgroundColor: Colors.green,
+                  content: Text('本を追加しました'),
+                );
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+              }
+              //画面を更新
+              model.fetchBookList();
+            },
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          );
+        }), // This trailing comma makes auto-formatting nicer for build methods.
       ),
     );
   }
